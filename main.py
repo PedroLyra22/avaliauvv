771a3446f0bc60ec44ec44b919e468fb25658aa2
Pedro <pedrocalezanelyra@gmail.com>
2025-05-05 19:50:09 -0300

    novas rotas para CRUD de estande e das avaliações

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, request
 
-from models import Endereco, Evento
+from avaliacao_estande_repository import AvaliacaoEstandeRepository
+from avaliacao_evento_repository import AvaliacaoEventoRepository
+from models import Endereco, Evento, Estande, AvaliacaoEvento, AvaliacaoEstande
 from db import DB
 from endereco_repository import EnderecoRepository
 from evento_repository import EventoRepository
+from estande_repository import EstandeRepository
 
 app = Flask(__name__)
 
@@ -11,6 +14,10 @@ db = DB()
 
 endereco_repo = EnderecoRepository(db)
 evento_repo = EventoRepository(db)
+estande_repo = EstandeRepository(db)
+avaliacao_evento_repo = AvaliacaoEventoRepository(db)
+avaliacao_estande_repo = AvaliacaoEstandeRepository(db)
+
 
 # CRUD DE ENDEREÇO
 @app.route('/endereco', methods=['POST'])
@@ -78,7 +85,7 @@ def deletar_evento(id):
 
 
 @app.route('/evento', methods=['GET'])
-def listar_eventos():
+def listar_evaentos():
     eventos = evento_repo.listar_todos()
     if not eventos:
         return jsonify({"message": "Nenhum evento encontrado!"}), 404
@@ -92,6 +99,79 @@ def listar_eventos():
         "descricao": evento['descricao'],
         "endereco_id": evento['endereco_id']
     } for evento in eventos]), 200
+
+
+# CRUD DE ESTANDE
+@app.route('/estande', methods=['POST'])
+def criar_estande():
+    data = request.get_json()
+    estande = Estande(
+        id=None,
+        nome=data['nome'],
+        tema=data['tema'],
+        imagem=data['imagem'],
+        descricao=data['descricao'],
+        evento_id=data['evento_id']
+    )
+    estande_repo.inserir(estande)
+    return jsonify({"message": "Estande criado com sucesso!"}), 201
+
+@app.route('/estande', methods=['GET'])
+def listar_estandes():
+    estandes = estande_repo.listar_todos()
+
+    print(estandes)
+
+    return jsonify([{
+            "id": estande.get('id'),
+            "nome": estande.get('nome'),
+            "tema": estande.get('tema'),
+            "imagem": estande.get('imagem'),
+            "descricao": estande.get('descricao'),
+            "evento_id": estande.get('evento_id')
+        }
+        for estande in estandes
+    ]), 200
+
+@app.route('/estande/<int:id>', methods=['DELETE'])
+def deletar_estande(id):
+    estande_repo.deletar(id)
+    return jsonify({"message": "Estande deletado com sucesso!"}), 200
+
+
+#INSERT DE AVALIACAO DO EVENTO
+@app.route('/avaliacao_evento', methods=['POST'])
+def criar_avaliacao_evento():
+    data = request.get_json()
+
+    avaliacao = AvaliacaoEvento(
+        id=None,
+        nota_equipe=data['nota_equipe'],
+        nota_infraestrutura=data['nota_infraestrutura'],
+        nota_organizacao=data['nota_organizacao'],
+        nota_experiencia=data['nota_experiencia'],
+        imagem_avaliacao=data.get('imagem_avaliacao'),
+        evento_id=data['evento_id']
+    )
+
+    avaliacao_evento_repo.inserir(avaliacao)
+    return jsonify({"message": "Avaliação criada com sucesso!"}), 201
+
+@app.route('/avaliacao_estande', methods=['POST'])
+def criar_avaliacao_estande():
+    data = request.get_json()
+
+    avaliacao_estande = AvaliacaoEstande(
+        id=None,
+        nota_apresentacao=data['nota_apresentacao'],
+        nota_ideia=data['nota_ideia'],
+        nota_experiencia=data['nota_experiencia'],
+        imagem_avaliacao=data.get('imagem_avaliacao'),
+        estande_id=data['estande_id']
+    )
+
+    avaliacao_estande_repo.inserir(avaliacao_estande)
+    return jsonify({"message": "Avaliação do estande criada com sucesso!"}), 201
 
 
 if __name__ == '__main__':
