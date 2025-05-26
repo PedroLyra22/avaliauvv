dabcaa4701958bf60dfc94629f5600a1ea7ce369
Pedro <pedrocalezanelyra@gmail.com>
2025-05-26 20:18:15 -0300

    feat: mudança no repositorio para criar a media dos estandes

--- avaliacao_estande_repository.py
+++ avaliacao_estande_repository.py
@@ -15,3 +15,27 @@ class AvaliacaoEstandeRepository:
                 avaliacao.estande_id
             ))
         self.conn.commit()
+
+    def media_por_estande(self, estande_id):
+        with self.conn.cursor() as cursor:
+            sql = """
+                SELECT 
+                    AVG(nota_apresentacao) AS media_apresentacao,
+                    AVG(nota_ideia) AS media_nota_ideia,
+                    AVG(nota_experiencia) AS media_nota_experiencia
+                FROM avaliacao_estande
+                WHERE estande_id = %s
+            """
+            cursor.execute(sql, (estande_id,))
+            resultado = cursor.fetchone()
+
+            if not resultado or all(v is None for v in resultado.values()):
+                return None
+
+            json_result = {
+                "estande_id": estande_id,
+                "media_apresentacao": round(resultado['media_apresentacao'], 2) if resultado['media_apresentacao'] is not None else None,
+                "media_nota_ideia": round(resultado['media_nota_ideia'], 2) if resultado['media_nota_ideia'] is not None else None,
+                "media_nota_experiencia": round(resultado['media_nota_experiencia'], 2) if resultado['media_nota_experiencia'] is not None else None
+            }
+            return json_result
